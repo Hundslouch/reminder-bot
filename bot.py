57a06ac2a288ac2514221f111f852911f4a7d7dd
Kseniya <kuznetsovakseniya23@gmail.com>
2024-11-11 03:32:22 +0300

    project base

---
+++ bot.py
@@ -0,0 +1,32 @@
+import time
+import logging
+import os
+from dotenv import load_dotenv
+from aiogram import Bot, Dispatcher, executor, types
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+load_dotenv()
+TOKEN = os.getenv("TOKEN")
+MSG = ", {} ,?"
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot=bot)
+
+
+@dp.message_handler(commands=["start"])
+async def start_handler(message: types.Message):
+    user_id = message.from_user.id
+    user_name = message.from_user.first_name
+    user_full_name = message.from_user.full_name
+    logging.info(f"{user_id=} {user_full_name=} {time.asctime()}")
+    await message.reply(f"Привет, {user_full_name}, я твой бот-напоминалка")
+
+    for i in range(10):
+        time.sleep(2)
+        await bot.send_message(user_id, MSG.format(user_name))
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp)
